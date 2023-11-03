9c852ddb0051c7d99f9a944dc1e34a913849be98
Silas Ørting <silas@protonmail.com>
2023-11-03 09:31:06 +0100

    Test that we can read a full dlf file

--- conftest.py
+++ conftest.py
@@ -1,6 +1,10 @@
 import pytest
 
 @pytest.fixture
+def dlf_path():
+    return 'test-data/DailyP-Daily-WaterFlux.dlf'
+
+@pytest.fixture
 def only_header_path():
     return 'test-data/DailyP-Soil-Tracer.dlf'
 
--- daisy_vis/io/test/test_dlf.py
+++ daisy_vis/io/test/test_dlf.py
@@ -1,19 +1,27 @@
 '''Test dlf module'''
 from daisy_vis.io import dlf
 
+def test_read_dlf(dlf_path):
+    '''Test that we can correctly read a dlf file'''
+    dlf_file = dlf.read_dlf(dlf_path)
+    assert len(dlf_file.header) > 0
+    assert len(dlf_file.units) > 0
+    assert len(dlf_file.units) == len(dlf_file.body.columns)
+
+
 def test_read_dlf_only_header_present(only_header_path):
     '''Test that we can correctly read a file without a body'''
-    soil_tracer = dlf.read_dlf(only_header_path)
-    assert len(soil_tracer.header) > 0
-    assert len(soil_tracer.units) == 0
-    assert len(soil_tracer.body) == 0
+    only_header = dlf.read_dlf(only_header_path)
+    assert len(only_header.header) > 0
+    assert len(only_header.units) == 0
+    assert len(only_header.body) == 0
 
 
 def test_read_dlf_empty_file(empty_file_path):
     '''Test that we dont blow up when reading an empty file'''
-    soil_tracer = dlf.read_dlf(empty_file_path)
-    assert len(soil_tracer.header) == 0
-    assert len(soil_tracer.units) == 0
-    assert len(soil_tracer.body) == 0
+    empty_file = dlf.read_dlf(empty_file_path)
+    assert len(empty_file.header) == 0
+    assert len(empty_file.units) == 0
+    assert len(empty_file.body) == 0
 
     
